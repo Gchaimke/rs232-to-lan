b21dd665b77c99dbe68ca6d245deec721383d9d0
Gchaimke <Gchaimke@gmail.com>
2016-06-01 09:07:50 +0300

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Resources
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Weller Zero-Smog")> 
+<Assembly: AssemblyDescription("Programm to check status of filter Weller Zero-Smog")> 
+<Assembly: AssemblyCompany("Avdor Helet")> 
+<Assembly: AssemblyProduct("Weller Zero-Smog")> 
+<Assembly: AssemblyCopyright("Copyright ©Avdor-Helet  2016")> 
+<Assembly: AssemblyTrademark("Avdor Helet")> 
+
+<Assembly: ComVisible(False)> 
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76355276-7182-4e8f-b56e-e2622964d68f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.2.0.0")> 
+
+<Assembly: NeutralResourcesLanguageAttribute("en")> 
